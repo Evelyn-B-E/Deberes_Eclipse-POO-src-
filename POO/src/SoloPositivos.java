afa8a781c13d5d8f2b13cd9e383ce1ee10da356e
Evelyn-B-E <67941068+Evelyn-B-E@users.noreply.github.com>
2020-07-09 17:43:17 -0500

    Add files via upload

---
+++ POO/src/SoloPositivos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SoloPositivos {
+
+	public static void main(String[] args) {
+		
+		
+		int promedio=10;
+		if(promedio>=7) {
+			int contador=10;
+			contador=9;
+			if(promedio <=9);{
+			   contador=10;
+		}
+		
+	System.out.println(contador);
+
+			}
+		}
+
+	}
